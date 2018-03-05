4f9e6b25d91e384b400d04a8ee7c880ec4fbe5d7
Jiayi Xu <joey.j.xu@pwc.com>
2018-03-05 11:03:03 +0800

    add side progress bar

--- scroll.js
+++ scroll.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
   // code here
+  $('#side-progress-bar').click(function () {
+    $('#progress-bar-entity').css({display: 'block'})
+    $('body').addClass('noScroll')
+  })
+
+  $(document).on('click', '.noScroll', function (evt) {
+    console.log('->: ', evt.target)
+    console.log(evt.target.className.indexOf('notClose'))
+    console.log('onScroll')
+    if (evt.target.className.indexOf('notClose') < 0) {
+      $('#progress-bar-entity').css({display: 'none'})
+      $('body').removeClass('noScroll')
+    }
+  })
 })
