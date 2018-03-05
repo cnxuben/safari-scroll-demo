36d2adfb601a479ae5fd0c4e7ac8c5d36a09820c
Jiayi Xu <joey.j.xu@pwc.com>
2018-03-05 14:51:17 +0800

    add close btn for the side progress bar

--- scroll.js
+++ scroll.js
@@ -5,13 +5,8 @@ $(document).ready(function () {
     $('body').addClass('noScroll')
   })
 
-  $(document).on('click', '.noScroll', function (evt) {
-    console.log('->: ', evt.target)
-    console.log(evt.target.className.indexOf('notClose'))
-    console.log('onScroll')
-    if (evt.target.className.indexOf('notClose') < 0) {
-      $('#progress-bar-entity').css({display: 'none'})
-      $('body').removeClass('noScroll')
-    }
+  $('#close-side-progress-bar').on('click', function () {
+    $('#progress-bar-entity').css({display: 'none'})
+    $('body').removeClass('noScroll')
   })
 })
